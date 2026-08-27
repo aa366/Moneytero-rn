ef3d8c818cf11e50d8d8548055bb65393ae9bd80
abdelwahab <bad3662067@gmail.com>
2026-08-27 08:29:40 +0300

    add: react-native-reusables + lucide icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./global.css";
@@ -6,5 +7,6 @@ export default function RootLayout() {
   return (
     <SafeAreaView className="flex-1">
       <Stack screenOptions={{ headerShown: false }} />
+      <PortalHost />
     </SafeAreaView>);
 }
--- src/components/Record.tsx
+++ src/components/Record.tsx
@@ -1,6 +1,6 @@
-import icon from "@/assets/images/icon.png";
 import { RecordType } from '@/types';
 import { Image, Text, View } from 'react-native';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 interface Props {
     data: RecordType;
@@ -19,17 +19,40 @@ export default function Record({
 }: Props) {
 
     return (
-        <View className=' flex flex-row gap-2 items-center mx-2 p-2 bg-gray-200'>
-            <Image source={icon} alt='' className="size-16 rounded-full " />
-            <View>
+
+        <Dialog>
+            <DialogTrigger>
+                <View className=' flex flex-row gap-2 items-center mx-2 p-2 bg-gray-200'>
+                    <Image source={require("@/assets/images/icon.png")} alt='' className="size-16 rounded-full " />
+                    <View>
 
 
+                        <Text>{`${from.name} => ${to.name}`}</Text>
+                        <Text>{id}</Text>
+                        <Text>{amount}</Text>
+                        {/* <Text>{time}</Text> */}
+                        {/* <Text>{note}</Text> */}
+                    </View>
+
+                </View>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogDescription>
+                        This action cannot be undone. This will permanently delete your account and remove your data
+                        from our servers.
+                    </DialogDescription>
+                </DialogHeader>
                 <Text>{`${from.name} => ${to.name}`}</Text>
                 <Text>{id}</Text>
                 <Text>{amount}</Text>
                 {/* <Text>{time}</Text> */}
                 {/* <Text>{note}</Text> */}
-            </View>
-        </View>
+            </DialogContent>
+
+
+
+        </Dialog>
     )
 }
